02c03310b6cdfc1899270f5243c91b8d09485f6f
Felipe Maia <felipeemaia4@gmail.com>
2024-04-03 19:24:37 -0300

    isometric CameraControler

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] float _speed;
+    private Vector3 _input;
+
+    void Update()
+    {
+        GatherInput();   
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void GatherInput()
+    {
+        Vector3 input = Vector3.zero;
+        input.x = Input.GetAxisRaw("Horizontal");
+        input.z = Input.GetAxisRaw("Vertical");
+        input.Normalize();
+
+        var isoMatrix = Matrix4x4.Rotate(Quaternion.Euler(0, 45, 0));
+        _input = isoMatrix.MultiplyPoint3x4(input);
+    }
+
+    private void Move()
+    {
+        Vector3 newPosition = transform.position + _input * _speed * Time.deltaTime;
+        transform.position = newPosition;
+    }
+}
