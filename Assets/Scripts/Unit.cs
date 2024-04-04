44deddd2f9e36fff2d049cc9a027d0ca9b581c35
Felipe Maia <felipeemaia4@gmail.com>
2024-04-04 18:20:54 -0300

    Unit animation and rotation
    - The unit changes animation when it's running or idle
    - The unit now rotates towards the tile it's walking to

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -8,6 +8,8 @@ public class Unit : MonoBehaviour
     public Tile tile;
     public GridBehaviour grid;
 
+    [SerializeField] Animator _anim;
+
     //Recieve a new path
     public void MoveUnit(List<Tile> path)
     {
@@ -17,15 +19,19 @@ public class Unit : MonoBehaviour
     //Move unit through each tile of the path
     public IEnumerator MoveThroughPath(List<Tile> path)
     {
+        _anim.SetTrigger("Run");
+
         while(path.Count > 1)
         {
             Vector3 startPos = path[0].transform.position;
             Vector3 endPos = path[1].transform.position;
             float lerpTime = 0;
 
+            StartCoroutine(RotateTowards(startPos, endPos));
+
             while (lerpTime < 1)
             {
-                lerpTime += 0.1f;
+                lerpTime += 0.05f;
                 transform.position = Vector3.Lerp(startPos, endPos, lerpTime);
                 yield return new WaitForFixedUpdate();
             }
@@ -35,6 +41,25 @@ public class Unit : MonoBehaviour
 
         tile = path[0];
         transform.position = tile.transform.position;
+        _anim.SetTrigger("Idle");
+    }
+
+    private IEnumerator RotateTowards(Vector3 startPos, Vector3 endPos)
+    {
+        Vector3 dist = endPos - startPos;
+        Quaternion startRotation = transform.rotation;
+        Quaternion desiredRotation = Quaternion.LookRotation(dist);
+
+        if (startRotation == desiredRotation) yield break;
+
+        float lerpTime = 0;
+
+        while (lerpTime < 1)
+        {
+            lerpTime += 0.1f;
+            transform.rotation = Quaternion.Lerp(startRotation, desiredRotation, lerpTime);
+            yield return new WaitForFixedUpdate();
+        }
     }
 
     public void Start()
