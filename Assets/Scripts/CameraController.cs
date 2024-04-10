a32d09a0329d7a41e00e269b050cceae1f7cf06d
Felipe Maia <felipeemaia4@gmail.com>
2024-04-10 09:06:34 -0300

    Camera focus on unit when it's turn starts

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,9 +1,16 @@
+using Brisanti.Tactics.Core;
+using Brisanti.Tactics.Managment;
+using System.Collections;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    [SerializeField] TurnManager _turnManager;
     [SerializeField] float _speed;
+    [SerializeField] float _focusTime;
+
     private Vector3 _input;
+    private bool _isFocusing;
 
     private void Update()
     {
@@ -28,7 +35,38 @@ public class CameraController : MonoBehaviour
 
     private void Move()
     {
+        if (_isFocusing) return;
         Vector3 newPosition = transform.position + _input * _speed * Time.deltaTime;
         transform.position = newPosition;
+    }
+
+    private void FocusUnit(Unit unit)
+    {
+        Vector3 targetPos = unit.transform.position;
+        targetPos.y = 0;
+        
+        StartCoroutine(FocusingOn(targetPos));
+    }
+
+    private IEnumerator FocusingOn(Vector3 endPos)
+    {
+        _isFocusing = true;
+        Vector3 startPos = transform.position;
+        float lerpTime = 0;
+
+        while (lerpTime < 1)
+        {
+            lerpTime += Time.fixedDeltaTime * _focusTime;
+            transform.position = Vector3.Slerp(startPos, endPos, lerpTime);
+            yield return new WaitForFixedUpdate();
+        }
+
+        _isFocusing = false;
+    }
+
+    private void Awake()
+    {
+        if(_turnManager)
+            _turnManager.OnTurnStart += FocusUnit;
     }
 }
